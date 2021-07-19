5f2fe91965b6596698ef3c0db49e6e0e8ff3ba72
Antonio Fittipaldi <moaiamorfo@users.noreply.github.com>
2021-07-19 13:34:45 +0200

    Aggiornata cartella resources.

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(
+	function(){
+		$("#vocemappa").hover(function() {
+  			$("#vocemappa").fadeOut("slow", function(){
+    			// alert("Il tag è stato nascosto correttamente.");
+  		});
+	});
+});
