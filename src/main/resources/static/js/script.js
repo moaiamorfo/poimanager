42ce9be7a9a7fcd7fb154aee796625caffafe2d3
Antonio Fittipaldi <moaiamorfo@users.noreply.github.com>
2021-07-22 14:00:22 +0200

    Integrazione JPA.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -6,25 +6,3 @@ $(document).ready(
   		});
 	});
 });
-
-function creaMappa() {
-	  var perimetro = new google.maps.LatLngBounds();
-	  const myLatLng = { lat: 40.4160721, lng: 16.8145609 };
-	  const map = new google.maps.Map(document.getElementById("mappa"), {
-	    zoom: 4,
-	    center: myLatLng,
-	  });
-	  new google.maps.Marker({
-	    position: myLatLng,
-	    map,
-	    title: "Tavole Palatine di Metaponto",
-	  });
-	  perimetro.extend(myLatLng);
-	  new google.maps.Marker({
-		position: { lat:40.6645789, lng:16.6115742 },
-		map,
-		title: "Sassi di Matera",
-	  });
-	  perimetro.extend({ lat:40.6645789, lng:16.6115742 });
-	  map.fitBounds(perimetro);
-	}
